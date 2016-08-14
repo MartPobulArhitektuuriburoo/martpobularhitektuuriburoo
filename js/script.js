3cb7ab114e76303c7728d0bd732153c4930353e3
Joonas Palm <joonas.palm.001@gmail.com>
2016-08-14 15:45:32 +0200

    Quote section. Smooth scrolling now considers the navbar offset.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 // Function for smooth scrolling on the page, when a navigation link is clicked.
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
+$(function () {
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var offset = 50;
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top - offset
+                }, 1000);
+                return false;
+            }
+        }
+    });
 });
